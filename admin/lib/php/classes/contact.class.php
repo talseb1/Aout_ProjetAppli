3499855a33ffeef6e70b1b3f3c1228b34d32b898
talseb1 <talseb0@hotmail.com>
2016-08-13 16:36:44 +0200

    Commit : creation class ~ support

---
+++ admin/lib/php/classes/contact.class.php
@@ -0,0 +1,30 @@
+<?php
+
+class contact {
+
+    private $_attributs = array();
+
+    public function __construct(array $data) {
+        $this->hydrate($data);
+    }
+
+    //hydrate
+    public function hydrate(array $data) {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        }
+    }
+
+    //getters
+    public function __get($nom) {
+        if (isset($this->_attributs[$nom])) {
+            return $this->_attributs[$nom];
+        }
+    }
+
+    //setters
+    public function __set($nom, $valeur) {
+        $this->_attributs[$nom] = $valeur;
+    }
+
+}
